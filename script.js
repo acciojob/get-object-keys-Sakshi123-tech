d654a938b6c2e444f934f0fcaf02518b105e7f06
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-02-23 20:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+// Step 2: Create the 'getKeys' function that accepts an object and returns an array of its keys.
+function getKeys(obj) {
+  return Object.keys(obj);  // Using Object.keys() to get the keys of the object
+}
+
+// Test the function
+console.log(getKeys(student));
